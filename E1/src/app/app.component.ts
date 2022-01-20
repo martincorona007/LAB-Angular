3d95dcf41112be0049538d534380b06f791e293b
martincorona007 <martincorona007@hotmail.com>
2022-01-20 07:47:40 -0600

    checking versions problems

--- E1/src/app/app.component.ts
+++ E1/src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  books : Books[];
-  index: number = 3;
-  constructor() { 
-     this.books = [
-        {id:1,name:"Angular"},
-        {id:2,name:"Typescript"},
-        {id:3,name:"Javascript"},
-        {id:4,name:"HTML"}
-     ];
-  }
-  
-  ngOnInit(): void {
-  }
-
-}
-
-interface Books{
-  id: number;
-  name : string;
+  title = 'E1';
 }
