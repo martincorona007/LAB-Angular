61564e0ba668500aa6ba85131c648b3fca1a9c0e
martincorona007 <martincorona007@hotmail.com>
2022-06-22 17:01:45 -0500

    adding files

--- E3/src/app/app.module.ts
+++ E3/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     MatInputModule,
     MatButtonModule,
-    FormsModule 
+    FormsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
